12c8ec277a729d793688909de4f6d8c165e34b3a
26blevine <26blevine@gannacademy.org>
2025-11-06 14:17:59 -0500

    lower

--- src/top_bands.py
+++ src/top_bands.py
@@ -3,7 +3,18 @@
 import pandas as pd
 
 def top_bands():
-    pass
+    bands = pd.read_csv("src/bands.tsv", sep="\t")
+    uk_top40 = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep="\t")
+    print(bands.head())
+    print(uk_top40.head())
+    bands["Band"] = bands["Band"].str.lower()
+    uk_top40["Artist"] = uk_top40["Artist"].str.lower()
+    merged_df = pd.merge(uk_top40, bands, left_on="Artist", right_on="Band")
+    print(merged_df.head())
+    #why is merged_df empty?
+
+    return merged_df
+
 
 def main():
     top_bands()
